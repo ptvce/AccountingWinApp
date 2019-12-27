629a62f282811c12caf548e3e59c542a9ca25d64
unknown <ptvce@yahoo.com>
2019-12-28 02:23:46 +0330

    use constructor injection and unit of work

--- Accounting/Accounting.DataLayer/Services/CustomerRepository.cs
+++ Accounting/Accounting.DataLayer/Services/CustomerRepository.cs
@@ -10,7 +10,11 @@ namespace Accounting.DataLayer.Services
 {
     public class CustomerRepository : ICustomerRepository
     {
-        Accounting_DBEntities db = new Accounting_DBEntities();
+        private Accounting_DBEntities db;
+        public CustomerRepository(Accounting_DBEntities context)
+        {
+            db = context;
+        }
         public bool DeleteCustomer(Customers custmer)
         {
             try
--- Accounting/ConsoleAppForTest/Program.cs
+++ Accounting/ConsoleAppForTest/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Accounting.DataLayer;
+using Accounting.DataLayer.Context;
 using Accounting.DataLayer.Repositories;
 using Accounting.DataLayer.Services;
 using System;
@@ -13,12 +14,11 @@ namespace ConsoleAppForTest
     {
         static void Main(string[] args)
         {
-            ICustomerRepository customer = new CustomerRepository();
+            UnitOfWork db = new UnitOfWork();
 
-            // test getAll
-            var list = customer.GetAllCustomers();
+            var list = db.CustomerRepository.GetAllCustomers();
+           
 
-            //test insert
             Customers newCustomer = new Customers()
             {
                 FullName = "سارا احمدی",
@@ -27,8 +27,26 @@ namespace ConsoleAppForTest
                 Address = "aaaa",
                 CustomerImage = "NoPhoto"
             };
-            customer.Insert(newCustomer);
-            customer.Save();
+            db.CustomerRepository.Insert(newCustomer);
+            db.CustomerRepository.Save();
+            db.Dispose();
+            //Accounting_DBEntities db = new Accounting_DBEntities();
+            //ICustomerRepository customer = new CustomerRepository();
+
+            //// test getAll
+            //var list = customer.GetAllCustomers();
+
+            ////test insert
+            //Customers newCustomer = new Customers()
+            //{
+            //    FullName = "سارا احمدی",
+            //    Mobile = "09122222323",
+            //    Email = "s@yahoo.com",
+            //    Address = "aaaa",
+            //    CustomerImage = "NoPhoto"
+            //};
+            //customer.Insert(newCustomer);
+            //customer.Save();
 
         }
     }
